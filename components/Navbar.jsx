e75e2906b396a05c35b9b4e97703b8bb6e9ddc69
Nahuelzwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-01-04 15:47:59 -0300

    abcd
    asd

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => (
         className="w-[34px] h-[34px] object-contain"
       />
       <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        CODEHUIT
+        CODE<span className="font-extrabold text-[24px] leading-[30px] color-code">HUIT</span>
       </h2>
       <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain"/>
     </div>
