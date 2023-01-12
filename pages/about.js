c1a08ec6486ebbd3596bfff7e773c5e046c5f2d5
Nahuelzwonko <91986497+Nahuelzwonko@users.noreply.github.com>
2023-01-12 20:19:04 -0300

    create imput type
    creacion de input type de codehuit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import react from "react";
+import styles from "../styles";
+import { Navbar } from "../components";
+
+const about = () =>{
+    return(
+<div>
+    <Navbar />
+</div>
+    )
+} 
+export default about;
