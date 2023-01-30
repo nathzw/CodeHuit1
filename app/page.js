82e2e55ebde163c2ab5bd3de6ff3311efbdabea6
nathzwonko <nathanielzwonko@gmail.com>
2023-01-30 07:45:22 -0300

    agrego alert para uso de cookies

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 import React from "react";
 import { Footer, Navbar } from "../components";
 import { useState, useEffect } from "react";
 import SyncLoader from "react-spinners/SyncLoader";
+import CookieConsent from "react-cookie-consent";
 import {
   About,
   Explore,
@@ -15,14 +16,13 @@ import {
   CTA,
 } from "../sections";
 
-
 function Page() {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 2000);
   }, []);
   return (
     <div className={`loader`}>
@@ -52,6 +52,31 @@ function Page() {
           <div className="relative mt-[23px]">
             <CTA />
           </div>
+          {/* alert cookies */}
+          <CookieConsent
+            debug={true}
+            location="bottom"
+            style={{ background: "#1a1a1a", textAlign: "center" }}
+            buttonStyle={{
+              color: "#fff",
+              background: "#3561ff",
+              fontSize: "14px",
+              border: "2px",
+              borderRadius:"10px",
+              width:"100px",
+              height:"30px"
+            }}
+            buttonText="Accept"
+            expires={365}
+            buttonClasses="btn btn-primary"
+          >
+            Este sitio utiliza cookies. Consulte nuestra{" "}
+            <a style={{ color: "#b4ff00" }} href="/terminos/Terminos">
+              política de privacidad
+            </a>{" "}
+            para obtener más información.
+          </CookieConsent>
+          {/* fin alert cookies */}
           <Footer />
         </div>
       )}
